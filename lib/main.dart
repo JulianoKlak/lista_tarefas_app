599d2f6094ab0d27ba396bb566c89c5fc7024884
JulianoKlak <julianoklak@gmail.com>
2025-09-15 21:37:15 -0300

    criando tela de tarefas

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,19 @@ class ListaTarefasApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) { //widget principal - material app
     return new MaterialApp(
-      home: new Scaffold(
+      home: new ListaScreen(),
+    ); //MaterialApp
+  }
+}
+
+class ListaScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
         appBar: new AppBar(
           title: new Text("Lista de Tarefas"),
         ),
         body: Column(), //Column
-      ) //Scaffold
-    ); //MaterialApp
+      ); //Scaffold
   }
 }
